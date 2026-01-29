6dad730ad008efb1acee5caccb7d26d606ca99e6
Albert P. <68952219+ap-jrzxc@users.noreply.github.com>
2026-01-29 13:26:07 +0800

    Added SQL dump file

---
+++ sql/model.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 29, 2026 at 06:24 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `model`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+-- Error reading structure for table model.article: #1932 - Table &#039;model.article&#039; doesn&#039;t exist in engine
+-- Error reading data for table model.article: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `model`.`article`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+-- Error reading structure for table model.employees: #1932 - Table &#039;model.employees&#039; doesn&#039;t exist in engine
+-- Error reading data for table model.employees: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `model`.`employees`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `icti`
+--
+-- Error reading structure for table model.icti: #1932 - Table &#039;model.icti&#039; doesn&#039;t exist in engine
+-- Error reading data for table model.icti: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `model`.`icti`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `office`
+--
+-- Error reading structure for table model.office: #1932 - Table &#039;model.office&#039; doesn&#039;t exist in engine
+-- Error reading data for table model.office: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `model`.`office`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+-- Error reading structure for table model.supplier: #1932 - Table &#039;model.supplier&#039; doesn&#039;t exist in engine
+-- Error reading data for table model.supplier: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `model`.`supplier`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table model.user: #1932 - Table &#039;model.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table model.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `model`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
